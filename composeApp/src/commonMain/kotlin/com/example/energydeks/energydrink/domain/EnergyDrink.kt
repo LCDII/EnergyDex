66ed7575eb5d9aba192e41e44971be5303a493f4
LCDII <piskunov.leon@gmail.com>
2026-03-29 08:04:55 +0300

    Initial commit: EnergyDeks

---
+++ composeApp/src/commonMain/kotlin/com/example/energydeks/energydrink/domain/EnergyDrink.kt
@@ -0,0 +1,8 @@
+package com.example.energydeks.energydrink.domain
+
+data class EnergyDrink(
+    val id: String,
+    val name: String,
+    val description: String,
+    val rating: Double?,
+)
---
+++ composeApp/src/iosMain/kotlin/com/example/energydeks/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.example.energydeks
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
